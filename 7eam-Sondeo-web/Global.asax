1cd5a3ba1e97834dbcb83a16173fc82e2ba589e5
Mauricio Orozco <maurialex_95@hotmail.com>
2019-11-22 01:10:51 -0600

    Agregar archivos de proyecto.

---
+++ 7eam-Sondeo-web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_7eam_Sondeo_web.MvcApplication" Language="C#" %>
---
+++ 7eam-Sondeo-web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_7eam_Sondeo_web.Startup))]
+namespace _7eam_Sondeo_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
